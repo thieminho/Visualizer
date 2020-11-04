173b7dc21ef5fabf43a2d39dc8644c6b5801662c
Milosz <49844410+thieminho@users.noreply.github.com>
2020-11-04 17:22:58 +0100

    Add files via upload

---
+++ app/app.py
@@ -0,0 +1,16 @@
+#from plugins import one, two
+
+import importlib
+PLUGIN_NAME = "plugins.two"  #zamiast tego znajdowanie modułów z folderu plugins
+
+plugin_module = importlib.import_module(PLUGIN_NAME, ".")
+
+print(plugin_module)
+
+plugin = plugin_module.Plugin("hello", key=1)
+result = plugin.execute(7,2)
+print(result)
+
+#print(one)
+#print(two)
+#one.Plugin("hello world", key=123)
