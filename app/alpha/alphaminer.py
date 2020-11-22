1f1a88513df494352babafdbd0461ce2d149faa7
JuliaCh <jchalasiak@gmail.com>
2020-11-22 18:47:51 +0100

    Init footprint matrix

--- app/alpha/alphaminer.py
+++ app/alpha/alphaminer.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 
 # TODO: Preprocessing logs to the form of case_id, act_name?
@@ -30,8 +31,34 @@ def find_sets(dataframe):
     print(all_events)
     print(start_events)
     print(end_events)
-    return
+    return all_events, start_events, end_events
+
+
+def create_footprint_matrix(dataframe):
+    all_events = list(dataframe.act_name.unique())
+    cases = dataframe.case_id.unique()
+    #all_events_dict = {v: k for v, k in enumerate(all_events)}
+    #print(all_events_dict)
+    foot_matrix = np.chararray((len(all_events), len(all_events)))
+    foot_matrix[:] = '0'
+    print(foot_matrix)
+    print(cases)
+    for case in cases:
+        df = dataframe[dataframe.case_id.eq(case)]
+        last_act_name = ''
+        for index, row in df.iterrows():
+            if last_act_name != '':
+                if foot_matrix[int(all_events.index(row['act_name'])) , int(all_events.index(last_act_name))] == '0':
+                    print('a')
+                elif foot_matrix[int(all_events.index(row['act_name'])) , int(all_events.index(last_act_name))] != '0':
+                    print('b')
+            last_act_name = row['act_name']
+    foot_matrix[foot_matrix == '0'] = '#'
+    print(foot_matrix)
+    return foot_matrix
+
 
 
 df = read_csv_into_df('test_log_june_1.csv')
 find_sets(df)
+create_footprint_matrix(df)
