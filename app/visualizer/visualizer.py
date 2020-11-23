fd71d1a4920bfc6978638803191e20e86faa8bcb
Błażej <baryla181@gmail.com>
2020-11-23 19:51:13 +0100

    PyQtWebEngine in app main window. Little layout improvement in Network (Visualizer class)

--- app/visualizer/visualizer.py
+++ app/visualizer/visualizer.py
@@ -8,7 +8,7 @@ class Visualizer(QWebEngineView):
     def __init__(self, graph):
         super().__init__()
         self.graph = graph
-        self.net = Network()
+        self.net = Network(height="100%", width="100%", notebook=False)
 
     def set_graph_to_network(self):
         self.net.from_nx(nx_graph=self.graph)
@@ -17,5 +17,3 @@ class Visualizer(QWebEngineView):
         self.net.save_graph("visuzalized_graph.html")
         self.setHtml(self.net.html)
         super().show()
-
-
