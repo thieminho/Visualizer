f5c355b38756486d8f55778ceff60da9e1e705c6
Błażej <baryla181@gmail.com>
2020-11-25 16:57:08 +0100

    PyQtWebEngine in app main window. Little layout improvement in Network (Visualizer class)

--- app/visualizer/visualizer.py
+++ app/visualizer/visualizer.py
@@ -8,10 +8,13 @@ class Visualizer(QWebEngineView):
     def __init__(self, graph):
         super().__init__()
         self.graph = graph
-        self.net = Network(height="100%", width="100%", notebook=False)
+        self.net = Network(height="100%", width="100%", notebook=False, directed=True)
+        self.net.set_edge_smooth(smooth_type='dynamic')
 
     def set_graph_to_network(self):
-        self.net.from_nx(nx_graph=self.graph)
+        for _,_,edge_attr in self.graph.edges(data=True):
+            edge_attr['value'] = edge_attr['Weight']
+        self.net.from_nx(nx_graph=self.graph, default_node_size=10, default_edge_weight=1)
 
     def show(self) -> None:
         self.net.save_graph("visuzalized_graph.html")
